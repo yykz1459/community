e0d1cc96fe24bbb2dbf11628f5235ffd6336f1c0
zhengwenyi <593802401@qq.com>
2020-06-06 17:16:38 +0800

    init repo

---
+++ src/test/java/com/zwy/community/comunity/ComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwy.community.comunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
